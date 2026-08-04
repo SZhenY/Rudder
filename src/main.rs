efd52ba665fc3b6c0bef26a8d4607dfe1ea762af
zhenY <s15779509684@outlook.com>
2026-08-04 10:44:26 +0800

    Fix --version output: lowercase rudder for CI match

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod webdav;
 
 fn main() -> anyhow::Result<()> {
     if std::env::args().any(|arg| arg == "--version" || arg == "-V") {
-        println!("Rudder {}", env!("CARGO_PKG_VERSION"));
+        println!("rudder {}", env!("CARGO_PKG_VERSION"));
         return Ok(());
     }
 
